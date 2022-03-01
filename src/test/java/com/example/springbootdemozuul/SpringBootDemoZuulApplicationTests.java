78580cf2ca6e6c9ba586b48825d9366f65b41f55
raffeynasser <raffey.nasser@starzplayarabia.com>
2022-03-01 16:32:49 +0500

    master: spring boot demo zuul

---
+++ src/test/java/com/example/springbootdemozuul/SpringBootDemoZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemozuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
